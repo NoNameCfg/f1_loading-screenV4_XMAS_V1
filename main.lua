a22c580c96c9f9b3b77f5d1e81250963bae17740
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2023-12-24 04:32:52 +0000

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,19 @@
+local loading_screen_start = false
+
+AddEventHandler("playerSpawned", function ()
+    if not loading_screen_start then
+        ShutdownLoadingScreenNui()
+        loading_screen_start = true
+    end
+end)
+print("F1 LOADING SCREEN V4 XMAS V1")
+print("-----------------------------------------------------------")
+print("THANKS FOR USE MY LOADING SCREEN JOIN THE DISCORD PLEASE :(")
+
+--[[
+  --------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                              THANKS FOR USE MY LOADING SCREEN JOIN THE DISCORD PLEASE :(
+
+  --------------------------------------------------------------------------------------------------------------------------------------------------
+--]]
